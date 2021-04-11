87c83513bbc168db2c52fd6f86624f8cb729bcc6
Micah Pellum <pellummicah@gmail.com>
2021-04-11 13:47:12 -0600

    set up schema files

--- solid-broccoli-master/Develop/server/schemas/typeDefs.js
+++ solid-broccoli-master/Develop/server/schemas/typeDefs.js
@@ -1,12 +1,20 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+
+
 // create our typeDefs
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    books: [Books]
   }
 `;
+
+type books {
+    _id: ID
+    username: String
+  }
+
 
 // export the typeDefs
 module.exports = typeDefs;
--- solid-broccoli-master/Develop/server/server.js
+++ solid-broccoli-master/Develop/server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
